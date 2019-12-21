61253bda59570bf3a7d4ea7921c00c6e2fb9baf6
liruifeng <1821975576@qq.com>
2019-12-21 20:43:30 +0800

    Create tongXie.vue

---
+++ ddcat_app/src/views/tongXie.vue
@@ -0,0 +1,188 @@
+
+<template>
+  <div class="tongXie">
+    <div class="header-logo">
+      <img src="../assets/xie1.jpg" />
+    </div>
+    <div class="header-right">
+      <ul>
+        <li class="clicknav clicknavnoton">
+          <a href="javascript:void(0)">
+            <img src="../assets/xie11.jpg" />
+          </a>
+        </li>
+        <li>
+          <a href="http://wei.ddcat.cn/app/./index.php?i=3&c=entry&eid=23">
+            <img src="../assets/xie6.jpg" />
+          </a>
+        </li>
+      </ul>
+    </div>
+
+    <!-- <div class="headerright">
+  <ul>
+    <li><a href="brand.php">品牌介绍</a><ul class="nav_sub"></ul></li><li><a href="news.php">新闻动态</a><ul class="nav_sub"></ul></li><li><a href="#">产品展示</a><ul class="nav_sub"><li><a href="#">婴小童</a><ul class="s"><li><a href="yxtcl.php">婴小童-陈列</a></li><li><a href="yxtcx.php">2019-春夏装</a></li><li><a href="yxtqd.php">2018-秋冬装</a></li></ul></li><li><a href="#">中大童</a><ul class="s"><li><a href="zdtcl.php">中大童-陈列</a></li><li><a href="zdtcx.php">2019-春夏款</a></li><li><a href="zdtqd.php">2018-暖冬款</a></li></ul></li><li><a href="qsct.php">潮童系列</a></li><li><a href="#">童鞋</a></li><li><a href="txps.php">配饰品</a></li></ul></li><li><a href="jmys.php">加盟优势</a><ul class="nav_sub"></ul></li><li><a href="sxy.php">商学院</a><ul class="nav_sub"></ul></li><li><a href="ssjd.php">时尚经典</a><ul class="nav_sub"></ul></li><li><a href="qsct.php">轻奢潮童</a><ul class="nav_sub"></ul></li><li><a href="mybh.php">母婴百货</a><ul class="nav_sub"></ul></li><li><a href="etly.php">儿童乐园</a><ul class="nav_sub"></ul></li><li><a href="txps.php">童鞋配饰</a><ul class="nav_sub"></ul></li><li><a href="contact.php">联系我们</a><ul class="nav_sub"></ul></li><li><a href="index.php">回到首页</a><ul class="nav_sub"></ul></li>  </ul>
+    </div>-->
+
+    <!-- /header-->
+
+    <div class="txps-1 jianbian wow fadeInUp">
+      <img
+        src="../assets/xie1.jpg"
+        class="wow fadeInUp"
+        data-wow-delay="0.2s"
+        data-wow-duration="0.4s"
+      />
+      <p>
+        叮当猫童装是叮当猫国际控股集团旗下广东叮当猫童鞋有限公司经营的童鞋品牌，
+        是一家集设计、研发、生产、营销于一体的运营企业。
+      </p>
+      <img
+        src="../assets/xie1.jpg"
+        class="wow fadeInUp"
+        data-wow-delay="0.2s"
+        data-wow-duration="0.4s"
+      />
+      <p>品牌风格：时尚、休闲</p>
+      <img
+        src="../assets/xie3.jpg"
+        class="wow fadeInUp"
+        data-wow-delay="0.2s"
+        data-wow-duration="0.4s"
+      />
+      <p>品牌理念：精心打造优质、舒适环保健康童鞋。时刻关心青少年儿童健康成长。</p>
+      <img
+        src="../assets/xie4.jpg"
+        class="wow fadeInUp"
+        data-wow-delay="0.2s"
+        data-wow-duration="0.4s"
+      />
+      <p>生产定位：高档定位，中档定位。适合中低消费群体，致力打造全球知名童鞋。</p>
+      <img
+        src="../assets/xie4.jpg"
+        class="wow fadeInUp"
+        data-wow-delay="0.2s"
+        data-wow-duration="0.4s"
+      />
+      <p>品牌定位：适合3-16岁儿童。</p>
+      <img
+        src="../assets/xie6.jpg"
+        class="wow fadeInUp"
+        data-wow-delay="0.2s"
+        data-wow-duration="0.4s"
+      />
+      <p>产品分类：运动鞋、休闲鞋、小白鞋、凉鞋、沙滩鞋、公主鞋......</p>
+      <img
+        src="../assets/xie7.jpg"
+        class="wow fadeInUp"
+        data-wow-delay="0.2s"
+        data-wow-duration="0.4s"
+      />
+      <p>
+        叮当猫鞋业部团队，坚持、稳重、精进、专注成长的核心价值观，客户所需，即我为的服务理念，
+        全心全意为“第一品牌”的愿景二努力！携手共创美好未来！
+      </p>
+      <img
+        src="../assets/xie8.jpg"
+        class="wow fadeInUp"
+        data-wow-delay="0.2s"
+        data-wow-duration="0.4s"
+      />
+      <img
+        src="../assets/xie9.jpg"
+        class="wow fadeInUp"
+        data-wow-delay="0.2s"
+        data-wow-duration="0.4s"
+      />
+      <img
+        src="../assets/xie10.jpg"
+        class="wow fadeInUp"
+        data-wow-delay="0.2s"
+        data-wow-duration="0.4s"
+      />
+      <img
+        src="../assets/xie11.jpg"
+        class="wow fadeInUp"
+        data-wow-delay="0.2s"
+        data-wow-duration="0.4s"
+      />
+      <img
+        src="../assets/xie12.jpg"
+        class="wow fadeInUp"
+        data-wow-delay="0.2s"
+        data-wow-duration="0.4s"
+      />
+    </div>
+    <div class="news-body-img">
+      <a href>
+        <img src="../assets/xie13.jpg" />
+      </a>
+      <a href>
+        <img src="../assets/xie14.jpg" />
+      </a>
+    </div>
+
+    <!-- /mainbody-->
+    <div class="index-form">
+      <div class="form-box">
+        <div class="left">
+          <h2>别拦我！我要加盟>></h2>
+          <div class="form">
+            <form>
+              <ul>
+                <li>
+                  <input type="text" placeholder="您的姓名" value class="wqname" />
+                </li>
+                <li>
+                  <input type="text" placeholder="您的电话" value class="wqphone" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+        <div class="right">
+          <h2>我们的专员会迅速联系您！</h2>
+          <a href="javascript:void(0)" class="gotoweiqing">提交信息</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  date(){
+    return{
+
+    }
+  }
+};
+</script>
+<style scope>
+/* .tongXie{
+        margin-top:0.8rem;
+    } */
+body {
+  font: size 0.2rem;
+}
+.header-logo {
+  width: 100%;
+}
+img {
+  width: 100%;
+}
+p {
+  font-size: 0.4rem;
+  font-weight: bold;
+  line-height: 0.6rem;
+  text-indent: 0.8rem;
+}
+h2 {
+  font-size: 0.7rem;
+  color: red;
+}
+.right a {
+  color: green;
+  font-weight: bold;
+}
+</style>
